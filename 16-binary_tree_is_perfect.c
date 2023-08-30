1709f3eb044ecd4b2ac871eb2e0d27934212cdd4
Magdalina1 <majdalol95@gmail.com>
2023-08-30 13:12:59 -0700

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -29,3 +29,28 @@ int tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 }
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect.
+ * @tree: tree to check
+ * Return: 1 is it is or 0 if not
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int result = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		result = tree_is_perfect(tree);
+		if (result != 0)
+		{
+			return (1);
+		}
+		return (0);
+	}
+}
