dd876cb6c8b02bda30f660314fe1045162dae7cf
Magdalina1 <majdalol95@gmail.com>
2023-08-31 08:57:33 -0700

    adding new func

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -96,3 +96,31 @@ int remove_type(bst_t *root)
 		return (new_value);
 	}
 }
+
+/**
+ * bst_remove - remove a node from a BST tree
+ * @root: root of the tree
+ * @value: node with this value to remove
+ * Return: the tree changed
+ */
+
+bst_t *bst_remove(bst_t *root, int value)
+{
+	int type = 0;
+
+	if (root == NULL)
+		return (NULL);
+	if (value < root->n)
+		bst_remove(root->left, value);
+	else if (value > root->n)
+		bst_remove(root->right, value);
+	else if (value == root->n)
+	{
+		type = remove_type(root);
+		if (type != 0)
+			bst_remove(root->right, type);
+	}
+	else
+		return (NULL);
+	return (root);
+}
