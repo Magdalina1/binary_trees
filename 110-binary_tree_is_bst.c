1d0a13219f638ab8e722f9f8d37d46fedc8b91e7
Magdalina1 <majdalol95@gmail.com>
2023-08-31 01:28:52 -0700

    add new func

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -27,3 +27,31 @@ int check_sub_tree_Left(const binary_tree_t *node, int max)
 		return (0);
 	}
 }
+
+/**
+ * check_sub_tree_Right - check if all the nodes are bigger than the
+ * root specified
+ * @node: node in the tree to verify condition
+ * @min: value to compare
+ * Return: 1 if all is bigger or equal or 0 if not
+ */
+
+int check_sub_tree_Right(const binary_tree_t *node, int min)
+{
+	int left = 0, right = 0;
+
+	if (node == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		if (node->n >= min)
+			return (0);
+		left = check_sub_tree_Right(node->left, min);
+		right = check_sub_tree_Right(node->right, min);
+		if (left == right && left == 1)
+			return (1);
+		return (0);
+	}
+}
