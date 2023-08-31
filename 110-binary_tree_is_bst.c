0ec960646b6c4b6e9fd94103e516ef2f0ee129d5
Magdalina1 <majdalol95@gmail.com>
2023-08-31 01:38:13 -0700

    110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -55,3 +55,45 @@ int check_sub_tree_Right(const binary_tree_t *node, int min)
 		return (0);
 	}
 }
+
+/**
+ * binary_tree_is_bst - says if a tree is a bst or not
+ * the process here is first verify that the left node be smaller than the root
+ * then verify if the right node is bigger than th root.
+ * after that verify if the left subtree has nodes smaller than root
+ * and the right subtree has bigger nodes than root
+ * @tree: node that point to the tree to check
+ * Return: 1 if it is a BST or 0 if not
+ */
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	int var = 0, left = 2, right = 2;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left && tree->left->n > tree->n)
+		return (0);
+	if (tree->right && tree->right->n < tree->n)
+		return (0);
+	if (tree->left && tree->left->n < tree->n)
+	{
+		var = check_sub_tree_Left(tree->left, tree->n);
+		if (var == 0)
+			return (0);
+		left = binary_tree_is_bst(tree->left);
+	}
+	if (tree->right && tree->right->n > tree->n)
+	{
+		var = check_sub_tree_Right(tree->right, tree->n);
+		if (var == 0)
+			return (0);
+		right = binary_tree_is_bst(tree->right);
+	}
+	if (left != 2 || right != 2)
+	{
+		if (left == 0 || right == 0)
+			return (0);
+	}
+	return (1);
+}
