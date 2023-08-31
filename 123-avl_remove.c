38033e20b32db99e720cff8f2a1c424c04534cb7
Magdalina1 <majdalol95@gmail.com>
2023-08-31 09:05:35 -0700

    123-avl_remove.c

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -124,3 +124,20 @@ bst_t *bst_remove(bst_t *root, int value)
 		return (NULL);
 	return (root);
 }
+
+/**
+ * avl_remove - remove a node from a AVL tree
+ * @root: root of the tree
+ * @value: node with this value to remove
+ * Return: the tree changed
+ */
+
+avl_t *avl_remove(avl_t *root, int value)
+{
+	avl_t *root_a = (avl_t *) bst_remove((bst_t *) root, value);
+
+	if (root_a == NULL)
+		return (NULL);
+	bal(&root_a);
+	return (root_a);
+}
