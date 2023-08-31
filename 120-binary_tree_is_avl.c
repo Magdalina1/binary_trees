ab951cead12de5334394fcbde7ea381bf239792f
Magdalina1 <majdalol95@gmail.com>
2023-08-31 04:54:30 -0700

    120-binary_tree_is_avl.c

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -56,3 +56,18 @@ int bal_avl(const binary_tree_t *tree, int lower, int high)
 	}
 	return (1);
 }
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL tree.
+ * @tree: node that point to the tree to check.
+ * Return: 1 if tree is AVL, 0 if not.
+ */
+
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	return (bal_avl(tree, INT_MIN, INT_MAX));
+}
