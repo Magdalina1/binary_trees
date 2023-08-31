d3da05a25ade6c396ad80a99793444b3c04375c4
Magdalina1 <majdalol95@gmail.com>
2023-08-31 06:56:06 -0700

    123-avl_remove.c

---
+++ 123-avl_remove.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * bal - Measures balance factor of a AVL
+ * @tree: tree to go through
+ * Return: balanced factor
+ */
+
+void bal(avl_t **tree)
+{
+	int bval;
+
+	if (tree == NULL || *tree == NULL)
+		return;
+	if ((*tree)->left == NULL && (*tree)->right == NULL)
+		return;
+	bal(&(*tree)->left);
+	bal(&(*tree)->right);
+	bval = binary_tree_balance((const binary_tree_t *)*tree);
+	if (bval > 1)
+		*tree = binary_tree_rotate_right((binary_tree_t *)*tree);
+	else if (bval < -1)
+		*tree = binary_tree_rotate_left((binary_tree_t *)*tree);
+}
