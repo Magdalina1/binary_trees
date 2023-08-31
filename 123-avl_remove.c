e120513a9dbe1e794160027c69cf7d13ea231745
Magdalina1 <majdalol95@gmail.com>
2023-08-31 07:09:11 -0700

    add func

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -22,3 +22,29 @@ void bal(avl_t **tree)
 	else if (bval < -1)
 		*tree = binary_tree_rotate_left((binary_tree_t *)*tree);
 }
+
+/**
+ * successor - get the next successor i mean the min node in the right subtree
+ * @node: tree to check
+ * Return: the min value of this tree
+ */
+
+int successor(bst_t *node)
+{
+	int left = 0;
+
+	if (node == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left = successor(node->left);
+		if (left == 0)
+		{
+			return (node->n);
+		}
+		return (left);
+	}
+
+}
